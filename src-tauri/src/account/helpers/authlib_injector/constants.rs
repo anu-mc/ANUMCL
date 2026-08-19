07ba0103c027134f94d171315980f96354ae9708
FloranceYeh <Florance_Sunrise@outlook.com>
2026-08-19 13:40:36 +0800

    fix(account): remove LittleSkin login entry

--- src-tauri/src/account/helpers/authlib_injector/constants.rs
+++ src-tauri/src/account/helpers/authlib_injector/constants.rs
@@ -3,18 +3,16 @@ pub const AHNUMC_AUTH_SERVER_URL: &str = "https://skin.ahnumc.org/api/yggdrasil"
 pub const AHNUMC_HOMEPAGE_URL: &str = "https://skin.ahnumc.org";
 pub const AHNUMC_YGGDRASIL_CONNECT_CONFIGURATION_URL: &str =
   "https://skin.ahnumc.org/.well-known/openid-configuration/oauth";
-pub static PRESET_AUTH_SERVERS: [&str; 3] = [
+pub static PRESET_AUTH_SERVERS: [&str; 2] = [
   AHNUMC_AUTH_SERVER_URL,
   "https://skin.mualliance.ltd/api/yggdrasil",
-  "https://littleskin.cn/api/yggdrasil",
 ];
 pub static SCOPE: &str =
   "openid offline_access Yggdrasil.PlayerProfiles.Select Yggdrasil.Server.Join";
 
-pub static CLIENT_IDS: [(&str, &str); 5] = [
+pub static CLIENT_IDS: [(&str, &str); 4] = [
   // built-in preset auth servers
   ("skin.mualliance.ltd", "27"),
-  ("littleskin.cn", "1014"),
   // supported MUA auth servers (ref: https://github.com/SJMC-Dev/SJMCL-client-ids)
   ("skin.jsumc.fun", "2"),
   ("skin.mc.taru.xj.cn", "6"),
